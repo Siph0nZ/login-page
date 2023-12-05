27d256f9c87c95332e5d2e1d98f794be33f4b09c
Siph0nZ <garrodmason@gmail.com>
2023-12-05 12:12:05 +0000

    fixed show passwords

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 // Function to hide/show password \\
 function pswdHide() {
     var x = document.getElementById("input");
+    var y = document.getElementById("input2");
+    var z = document.getElementById("input3");
+
     if (x.type === "password") {
       x.type = "text";
     } else {
       x.type = "password";
     }
+
+    if (y.type === "password") {
+      y.type = "text";
+    } else {
+      y.type = "password";
   }
+
+  if (z.type === "password") {
+    z.type = "text";
+  } else {
+    z.type = "password";
+  }
+}
 
 // Switches between login and create account forms \\
 document.addEventListener("DOMContentLoaded", () => {
@@ -24,4 +39,4 @@ document.addEventListener("DOMContentLoaded", () => {
         loginForm.classList.remove("form-hidden");
         createAccountForm.classList.add("form-hidden");
     });
-})      
+})
